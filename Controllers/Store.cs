a07fd575b2671d8d04e0363550315fcf50527f71
raddevus <1radeutsch1@gmail.com>
2020-06-28 22:37:37 -0400

    Added Store class

---
+++ Controllers/Store.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class Store{
+    Int32 ID {get;set;}
+    Int32 MainKeyId {get;set;}
+    String Data {get;set;}
+    DateTime Created {get;set;}
+    DateTime Updated {get;set;}
+    bool Active{get;set;}
+}
