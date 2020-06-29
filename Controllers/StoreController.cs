4323d642c350aea23184faecdee1e98cf8ed6864
raddevus <1radeutsch1@gmail.com>
2020-06-28 22:19:22 -0400

    Initial project creation and alteration of StoreController from WeatherForecast controller.

---
+++ Controllers/StoreController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace LibreStore.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StoreController : ControllerBase
+    {
+        private readonly ILogger<StoreController> _logger;
+
+        public StoreController(ILogger<StoreController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<MainToken> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new MainToken
+            {
+                Key = rng.Next(-20, 55).ToString(),
+                Created = DateTime.Now.AddDays(index),
+                Active = true
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ MainToken.cs
@@ -0,0 +1,7 @@
+using System;
+
+public class MainToken{
+    public String Key{get; set;}
+    public DateTime Created {get;set;}
+    public bool Active{get;set;}
+}
