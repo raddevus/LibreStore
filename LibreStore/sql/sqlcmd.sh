714144cc582914d541f6d85a4c1003495d5e9544
raddevus <1radeutsch1@gmail.com>
2024-10-29 15:26:09 -0400

    added full path to sqlcmd (on docker image)

--- LibreStore/sql/sqlcmd.sh
+++ LibreStore/sql/sqlcmd.sh
@@ -1,3 +1,3 @@
 # use the following command to start sqlcmd on Docker
 # -No means no encryption so connection will succeed
-sqlcmd -S localhost -U SA -P $1 -No
+/opt/mssql-tools18/bin/sqlcmd -S localhost -U SA -P $1 -No
