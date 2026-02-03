08627b0e7be8fc22a835a226ec3bd1f3f347a4f6
raddevus <1radeutsch1@gmail.com>
2026-02-03 16:05:01 -0500

    Added pipe delimeter and column widths to sqlcmd for better output

--- LibreStore/sql/sqlcmd.sh
+++ LibreStore/sql/sqlcmd.sh
@@ -1,3 +1,4 @@
 # use the following command to start sqlcmd on Docker
 # -No means no encryption so connection will succeed
-/opt/mssql-tools18/bin/sqlcmd -S localhost -U SA -P $1 -No
+# -W -s "|" sets column widths & pipe delimeter between columns
+/opt/mssql-tools18/bin/sqlcmd -S localhost -U SA -P $1 -No -W -s "|"
